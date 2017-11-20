4a58cefac8d20446de45762314a84f245bd0be2c
dorian <uulake@outlook.com>
2017-11-20 03:09:34 -0500

    vgg fine tune all dense

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+from keras.preprocessing import image
+
+
+def get_accuracy(model, batches):
+    return model.evaluate_generator(batches, batches.samples)[1]
+
+
+def get_batches(path, gen=image.ImageDataGenerator(), target_size=(224, 224), class_mode='categorical', **kwargs):
+    return gen.flow_from_directory(path, target_size=target_size,
+                                   class_mode=class_mode, **kwargs)

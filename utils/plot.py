fdba42242294018c261b061efaee4851ea706a73
Dorian Wang <uulake@outlook.com>
2018-02-27 15:43:21 +0800

    add confusion matrix

--- utils/plot.py
+++ utils/plot.py
@@ -1,12 +1,51 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
+import itertools
+import numpy as np
 
 
-def plot_images(images, columns=4):
-    fig = plt.figure(figsize=(20, 10))
+def plot_images(images, titles=None, columns=4, figsize=(16, 10)):
+    fig = plt.figure(figsize=figsize)
     rows = int(len(images) / columns) + 1
 
     for idx, img in enumerate(images):
-        fig.add_subplot(rows, columns, idx + 1)
+        ax = fig.add_subplot(rows, columns, idx + 1)
+        if titles is not None:
+            ax.set_title(str(titles[idx]))
         plt.imshow(img)
     plt.show()
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
